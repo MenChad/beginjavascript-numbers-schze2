ee9f31712ec28b78900531e3e1217e6fa13b799d
MenChad <maditarik56@gmail.com>
2024-01-20 16:19:57 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 // 🦁 Import cette function
-// import { prompt } from "./helper.js";
+import { prompt } from "./helper.js";
 
 console.log("ADDITION-MASTER ™️");
 
 // 🦁 Utilise la function "prompt" pour demander le premier nombre à l'utilisateur
+
 // 🦁 Stocke le résultat dans une variable "firstNumber" et utilise `Number()` pour convertir la valeur en nombre
+let firstNumber = Number(prompt("un premier nombre ?"));
 // 💡 await prompt('Enter your name: ') permet de récupérer la valeur saisie par l'utilisateur
-
+let name = await prompt("Enter your name:");
 // 🦁 Fait la même chose pour la variable "secondNumber"
-
+let secondNumber = Number(prompt("un second nombre ?"));
 // 🦁 Affiche l'addition des deux nombres
+let result = firstNumber + secondNumber ;
+
+
